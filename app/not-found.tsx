a1e855dcc419d6ab89eeda03bc15d992ad385cb6
Munadil16 <munadilxd@gmail.com>
2024-09-18 15:05:54 +0530

    fix: Fix responsiveness of not-found page for mobiles

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,13 +4,15 @@ import { Button } from "@/components/ui/button";
 export default function NotFound() {
   return (
     <main className="flex min-h-[80svh] flex-col items-center justify-center gap-6">
-      <h1 className="text-6xl font-semibold">Page not found (404)</h1>
+      <h1 className="text-center text-4xl font-semibold sm:text-6xl">
+        Page not found (404)
+      </h1>
 
-      <p className="font-medium text-neutral-500">
+      <p className="text-sm font-medium text-neutral-500 sm:text-base">
         Sorry, we couldn&apos;t find the page you&apos;re looking for.
       </p>
 
-      <Button className="font-medium" asChild>
+      <Button className="p-6 text-base font-medium" asChild>
         <Link href="/">Go back home</Link>
       </Button>
     </main>
